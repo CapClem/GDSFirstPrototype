1b04e5287dd2d721b18d0e172c36fc4845b5318f
Jacob Clements <1014906@student.sae.edu.au>
2019-09-24 13:26:37 +0800

    Basic Beginnings
    Added initial scripts, basic game layout.

---
+++ Robot Regulator/Assets/Robot.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Robot : MonoBehaviour
+{
+    public GameObject idlePos;
+    public int robotHealth;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Add state machine states 
+        //idle
+        //task1 (substates ie travelling to location, completing task, returning to idlePos)
+        //task2 ""
+        //task3 ""
+        //malfunction
+        //destroyed
+        
+        //if state = idle
+        //return to robot idle area
+
+        //if 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
