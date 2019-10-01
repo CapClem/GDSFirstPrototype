80aaa12a44832581fc1c08fb73f8895961091f98
Bryce Mash <1013537@student.sae.edu.au>
2019-10-01 13:07:42 +0800

    NAvMesh & Tasks
    NavMesh clean up and tasks scripts added

--- Robot Regulator/Assets/ButtonActions.cs
+++ Robot Regulator/Assets/ButtonActions.cs
@@ -2,8 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ButtonHandler : MonoBehaviour
+public class ButtonsUI : MonoBehaviour
 {
+    
+    
+    
+    
+    
+    
     // Start is called before the first frame update
     void Start()
     {
