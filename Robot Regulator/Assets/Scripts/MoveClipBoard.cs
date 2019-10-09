23fd86a667cf2f86e1953ab2b233c5ab8340f90d
Karl Eyre <karl_eyre@hotmail.com>
2019-10-09 18:06:33 +1100

    Clip board Animations added
    Press Z to trigger clipboard. 
    Also added buttons for task to be completed.

---
+++ Robot Regulator/Assets/Scripts/MoveClipBoard.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveClipBoard : MonoBehaviour
+{
+    //Target the clip board
+    public GameObject clipBoard;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //test trigger for clipboard animation
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            OpinClipBoard();
+        }
+    }
+
+    //Change the bool on the clipboard animator to start the animation
+    public void OpinClipBoard()
+    {
+        Animator animator = clipBoard.GetComponent<Animator>();
+        {
+            if (animator != null)
+            {
+                bool OpenClipBoard = animator.GetBool("OpenClipBoard");
+                animator.SetBool("OpenClipBoard", !OpenClipBoard);
+            }
+        }
+    }
+}
