0a8ca925b8cc13eb434a109c192034639957c319
Jacob <1014906@student.sae.edu.au>
2019-10-11 18:27:34 +1100

    Got health bar up
    Got health bar and punish button to work together

---
+++ Robot Regulator/Assets/Scripts/RobotHealth.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RobotHealth : MonoBehaviour
+{
+    public Slider robotHealthBar;
+    public GameObject robot; 
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        robotHealthBar.value = 3;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    public void decreaseHealth()
+    {
+        robotHealthBar.value -= 1;
+        robot.GetComponent<DropItem>().failedTasks = robot.GetComponent<DropItem>().failedTasks += 1; 
+    }
+
+    
+}
